8e6f6e3745609636e02e99296b0cd5cae334e6d9
vagrant <vagrant@homestead>
2020-12-14 03:26:18 +0000

    admin change

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Iot',
+    'name' => 'Comlibra_Iot',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>Comlibra</b> admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>CLE</b>',
 
     /*
     |--------------------------------------------------------------------------
