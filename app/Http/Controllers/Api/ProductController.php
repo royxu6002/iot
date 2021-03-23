754f1fb7a845512fc69496f2df8bfa11fe69f2f9
vagrant <vagrant@homestead>
2021-03-23 13:03:41 +0000

    optiomize stocks api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -44,9 +44,6 @@ class ProductController extends Controller
     // 拿到所有产品的库存数据;
     public function getStocksData()
     {
-        return Product::whereHas('stocks')->with('stocks')->get();
+        return Product::has('stocks')->with('stocks')->get();
     }
-
-
-
 }
